e0e3fb6098bbc8aa00495aae0729c97021e4c162
sergeyskypro <shlyapnikov-90@mail.ru>
2024-09-30 22:09:02 +0300

    lesson_1_task_12345

--- 01_lesson/lesson_1_task_3.py
+++ 01_lesson/lesson_1_task_3.py
@@ -1,3 +1,3 @@
-first_name = input ("sergey")
-last_name = input ("skypro")
-print("вас зовут" + first_name + last_name)
+first_name = input("sergey: ")
+last_name = input("skypro: ")
+print("Вас зовут: " + last_name + " " + first_name)
