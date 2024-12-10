983040c9a33f43c006b2807bfe0cb50809b5f1fe
sergeyskypro <shlyapnikov-90@mail.ru>
2024-12-10 16:24:51 +0300

    Lesson_9

--- Lesson_9/09_lesson/Data.py
+++ Lesson_9/09_lesson/Data.py
@@ -1,22 +1,29 @@
-from sqlalchemy import create_engine
-from sqlalchemy.sql import text # noqa
+from sqlalchemy import create_engine, text
 
 
 class Data:
-    __scripts = {
-        "insert into": text("INSERT INTO subject(\"subject_title\", \"subject_id\") VALUES (:subject_title, subject_id"), # noqa
-        "delete by id": text("DELETE FROM subject WHERE subject_id = :id_to_delete"), # noqa
-        "get max id": "select MAX(subject_id) from subject"
-    }
-
     def __init__(self, connection_string):
         self.__db = create_engine(connection_string)
 
     def create(self, subject_title, subject_id):
-        self.__db.execute(self.__scripts("insert into"), subject_title=subject_title, subject_id=subject_id) # noqa
+        with self.__db.begin() as conn:
+            conn.execute(text("INSERT INTO subject(\"subject_title\", \"subject_id\") VALUES (:subject_title, :subject_id)"), # noqa
+                         {"subject_title": subject_title, "subject_id": subject_id} # noqa
+                         )
+
+    def update(self, subject_title, subject_id):
+        with self.__db.begin() as conn:
+            conn.execute(text("UPDATE subject SET subject_title = :update_title WHERE subject_id = :id_to_update"), # noqa
+                         {"update_title": subject_title, "id_to_update": subject_id} # noqa
+                         )
 
     def delete(self, id):
-        self.__db.execute(self.__scripts("delete by id"), id_to_delete=id)
+        with self.__db.begin() as conn:
+            conn.execute(text("DELETE FROM subject WHERE subject_id = :id_to_delete"), # noqa
+                         {"id_to_delete": id}
+                         )
 
     def get_max_id(self):
-        return self.__db.execute(self.__scripts("get max id")).fetchall()[0][0]
+        with self.__db.begin() as conn:
+            result = conn.execute(text("SELECT MAX(subject_id) FROM subject"))
+            return result.scalar()
